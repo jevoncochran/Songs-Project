14b686adfc119ad8c61357f8525493787a36afb5
Jevon Cochran <jevon@Jevons-MacBook-Pro.local>
2020-02-13 15:42:01 -0300

    latest edits

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ server.use(express.json());
 server.get('/', (req, res) => {
     
     const songs = [{ id: 1, artist: 'Roddy Rich', name: 'The Box' }];
-    
+
     res.status(200).json(songs);
 })
 
 
-const port = 5000; 
+const port = process.env.PORT || 5000; 
 server.listen(port, () => console.log(`\n*** API on port ${port} ***\n`));
